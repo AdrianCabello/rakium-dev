960d4f8e19d79e5bb0b6a7a75ba9e89cde965a52
Adrian Cabello <AdrianCabello@users.noreply.github.com>
2026-05-21 17:49:28 -0300

    fix: point Rakium frontend to production API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rakium-be-production.up.railway.app/api',
+  apiUrl: 'https://api.rakium.dev/api',
   rakiumClientId: 'a49284fc-c5cc-471e-a4f7-1fb71925c1e3',
 };
